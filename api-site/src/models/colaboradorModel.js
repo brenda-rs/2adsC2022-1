732f8d79df21fc32bb388fd6bcfc2cd17b905f04
mtorokm <MATHEUS.TOROK@bandtec.com.br>
2022-04-21 19:35:47 -0300

    Adicionando login e cadastro colaboradores

---
+++ api-site/src/models/colaboradorModel.js
@@ -0,0 +1,32 @@
+var database = require("../database/config")
+
+/* function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+} 
+ */
+function cadastrar(nome, nivel_acesso, funcao, telefone,email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    var instrucao = `
+        INSERT INTO colaborador (nome, nivel_acesso, funcao, telefone, email,senha, fk_empresa) VALUES ('${nome}','${nivel_acesso}','${funcao}','${telefone}','${email}', '${senha}',1);
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+function entrar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `    
+        SELECT id_colaborador,email, nome FROM colaborador 
+        WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+module.exports = {
+    cadastrar,
+    entrar
+};

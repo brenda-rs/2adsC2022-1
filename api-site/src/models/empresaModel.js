1cb85be8ffdfe0df5d15532700752c4e14fa40e0
mtorokm <MATHEUS.TOROK@bandtec.com.br>
2022-05-29 18:39:11 -0300

    atualizando empresa arrumado

--- api-site/src/models/empresaModel.js
+++ api-site/src/models/empresaModel.js
@@ -48,7 +48,7 @@ function puxar_dados_empresa(id_empresa) {
 function atualizarEmpresa(razao_social, cnpj, telefone, id_empresa) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
-    update empresa set razao_social = '${razao_social}', cnpj = ${cnpj}, telefone = ${telefone} where id_empresa = ${id_empresa};
+    update empresa set razao_social = '${razao_social}', cnpj = '${cnpj}', telefone = ${telefone} where id_empresa = ${id_empresa};
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
